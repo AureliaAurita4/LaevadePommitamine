0dd8997b832473cc557b1c22266a86a849dceea7
AureliaAurita4 <nyah88@gmail.com>
2016-10-01 00:00:42 +0300

    Update HelloWorld.java

--- src/Main/HelloWorld.java
+++ src/Main/HelloWorld.java
@@ -2,5 +2,6 @@ public class HelloWorld {
 	public static void main(String args[]) {
 		System.out.println("Hello, World!");
 		System.out.println("Author: Svetlana Jugai");
+		System.out.println("1 October 2016");
 	}
 }
